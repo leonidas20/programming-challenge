9ae6e5fed99a589308c5f2a48eda3d6e25e22b06
Lz <Lz@DESKTOP-SQQONGE>
2019-03-07 10:52:26 +0100

    4th Commit: 1. relative path of file 2. reading the file and printing the max and min temperature

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,11 @@
 package de.exxcellent.challenge;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -17,7 +23,27 @@ public final class App {
         // Your preparation code …
     	//1st commit
     	//2nd commit
+    	//3rd commit
 
+      String csvFile = "./src/main/resources//de/exxcellent/challenge//weather.csv";
+
+    	String line = "";
+        String cvsSplitBy = ",";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+
+            while ((line = br.readLine()) != null) {
+
+                // use comma as delimiter
+                String[] columns = line.split(cvsSplitBy);
+
+                System.out.println("Day " + columns[0]+" maximum temperature " + columns[1] + " , minimum temperature " + columns[2] + "]");
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     	
     	
         String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
@@ -25,5 +51,6 @@ public final class App {
 
         String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+        
     }
 }
